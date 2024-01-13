5b28b378755afb1f9571a21d8e64c8b6a6c0decc
Fausto Savoya <savoyafausto@gmail.com>
2024-01-12 22:02:49 -0300

    Tags & Notes Services code improvement

--- NotesApp/Services/Implementations/NotesServices.cs
+++ NotesApp/Services/Implementations/NotesServices.cs
@@ -28,23 +28,20 @@ namespace NotesApp.Services.Implementations
 
             var usuario = _notesContext.Users.Find(userId);
 
-            if (usuario != null)
-            {
-
-                Note nuevaNota = new Note
-                {
-                    Title = note.Title,
-                    UserId = userId,
-
-                };
-                _notesContext.Notes.Add(nuevaNota);
-                _notesContext.SaveChanges();
-            }
-            else
+            if (usuario == null)
             {
                 throw new Exception("El usuario no existe.");
+
             }
 
+            Note nuevaNota = new Note
+            {
+                Title = note.Title,
+                UserId = userId,
+
+            };
+            _notesContext.Notes.Add(nuevaNota);
+            _notesContext.SaveChanges();
 
         }
 
@@ -52,195 +49,188 @@ namespace NotesApp.Services.Implementations
         {
 
             var usuario = _notesContext.Users.Find(userId);
-
-            if (usuario != null)
-            {
-
-                var nota = _notesContext.Notes.FirstOrDefault(n => n.NoteId == noteId);
-
-                if (nota != null)
-                {
-
-                    nota.IsArchived = true;
-                    _notesContext.SaveChanges();
-                }
-                else
-                {
-
-                    throw new Exception("No existe ninguna nota con ese ID.");
-                }
-            }
-            else
-            {
-
-                throw new Exception("No existe un usuario con ese ID.");
-            }
-        }
-
-        public void DeletePermanently(int noteId, int userId)
-        {
-
-            var usuario = _notesContext.Users.Find(userId);
-
-            if (usuario != null)
-            {
-
-                var nota = _notesContext.Notes.FirstOrDefault(n => n.NoteId == noteId);
-
-                if (nota != null)
-                {
-
-                    _notesContext.Notes.Remove(nota);
-                    _notesContext.SaveChanges();
-                }
-                else
-                {
-
-                    throw new Exception("La nota no existe para el usuario especificado.");
-                }
-            }
-            else
-            {
-
-                throw new Exception("No existe un usuario con ese ID.");
-            }
-        }
-
-        public void EditNote(int noteId, int userId, NoteUpdateDto editNote)
-        {
-            var usuario = _notesContext.Users.Find(userId);
             var nota = _notesContext.Notes.FirstOrDefault(n => n.NoteId == noteId);
+
             if (usuario == null)
             {
-                throw new Exception("El usuario no existe.");
+                throw new Exception("No existe un usuario con ese ID.");
             }
 
             if (nota == null)
             {
-                throw new Exception("La nota no existe para el usuario especificado.");
-            }
-
-            if (editNote == null)
-            {
-                throw new Exception("Mal formato o directamente nulo.");
+                throw new Exception("No existe ninguna nota con ese ID.");
 
             }
-
-            nota.Title = editNote.Title ?? nota.Title;
-            nota.IsActive = editNote.IsActive ?? false;
-            nota.IsArchived = editNote.IsArchived ?? false;
-
-            if (editNote.Tag != null)
-            {
-                var tag = nota.Tags.FirstOrDefault(t => t.TagId == editNote.Tag);
-
-                if (tag == null)
-                {
-                    throw new Exception($"La etiqueta con ID {editNote.Tag} no está asociada a la nota.");
-                }
-                nota.Tags.Remove(tag);
-            }
-
+            nota.IsArchived = true;
             _notesContext.SaveChanges();
         }
+                    
+        
 
+    public void DeletePermanently(int noteId, int userId)
+    {
 
+        var usuario = _notesContext.Users.Find(userId);
 
-
-
-        public void RemoveNote(int noteId, int userId)
+        if (usuario != null)
         {
 
-            var usuario = _notesContext.Users.Find(userId);
+            var nota = _notesContext.Notes.FirstOrDefault(n => n.NoteId == noteId);
 
-            if (usuario != null)
+            if (nota != null)
             {
 
-                var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
-
-                if (nota != null)
-                {
-
-                    nota.IsActive = false;
-
-
-                    _notesContext.SaveChanges();
-                }
-                else
-                {
-
-                    throw new Exception("La nota no existe para el usuario especificado.");
-                }
+                _notesContext.Notes.Remove(nota);
+                _notesContext.SaveChanges();
             }
             else
             {
 
-                throw new Exception("El usuario no existe.");
+                throw new Exception("La nota no existe para el usuario especificado.");
             }
         }
-
-        public void UnarchiveNote(int noteId, int userId)
+        else
         {
 
-            var usuario = _notesContext.Users.Find(userId);
-
-            if (usuario != null)
-            {
-
-                var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
-
-                if (nota != null)
-                {
-
-                    nota.IsArchived = false;
-
-                    _notesContext.SaveChanges();
-                }
-                else
-                {
-
-                    throw new Exception("La nota no existe para el usuario especificado.");
-                }
-            }
-            else
-            {
-
-                throw new Exception("El usuario no existe.");
-            }
-        }
-
-        public void RestoreNote(int userId, int noteId)
-        {
-
-            var usuario = _notesContext.Users.Find(userId);
-
-            if (usuario != null)
-            {
-
-                var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
-
-                if (nota != null)
-                {
-
-                    nota.IsActive = true;
-
-
-                    _notesContext.SaveChanges();
-                }
-                else
-                {
-
-                    throw new Exception("La nota no existe para el usuario especificado.");
-                }
-            }
-            else
-            {
-
-                throw new Exception("El usuario no existe.");
-            }
-
-
-
+            throw new Exception("No existe un usuario con ese ID.");
         }
     }
+
+    public void EditNote(int noteId, int userId, NoteUpdateDto editNote)
+    {
+        var usuario = _notesContext.Users.Find(userId);
+        var nota = _notesContext.Notes.FirstOrDefault(n => n.NoteId == noteId);
+        if (usuario == null)
+        {
+            throw new Exception("El usuario no existe.");
+        }
+
+        if (nota == null)
+        {
+            throw new Exception("La nota no existe para el usuario especificado.");
+        }
+
+        if (editNote == null)
+        {
+            throw new Exception("Mal formato o directamente nulo.");
+
+        }
+
+        nota.Title = editNote.Title ?? nota.Title;
+        nota.IsActive = editNote.IsActive ?? false;
+        nota.IsArchived = editNote.IsArchived ?? false;
+
+        if (editNote.Tag != null)
+        {
+            var tag = nota.Tags.FirstOrDefault(t => t.TagId == editNote.Tag);
+
+            if (tag == null)
+            {
+                throw new Exception($"La etiqueta con ID {editNote.Tag} no está asociada a la nota.");
+            }
+            nota.Tags.Remove(tag);
+        }
+
+        _notesContext.SaveChanges();
+    }
+
+
+
+
+
+    public void RemoveNote(int noteId, int userId)
+    {
+
+        var usuario = _notesContext.Users.Find(userId);
+
+        if (usuario != null)
+        {
+
+            var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
+
+            if (nota != null)
+            {
+
+                nota.IsActive = false;
+
+
+                _notesContext.SaveChanges();
+            }
+            else
+            {
+
+                throw new Exception("La nota no existe para el usuario especificado.");
+            }
+        }
+        else
+        {
+
+            throw new Exception("El usuario no existe.");
+        }
+    }
+
+    public void UnarchiveNote(int noteId, int userId)
+    {
+
+        var usuario = _notesContext.Users.Find(userId);
+
+        if (usuario != null)
+        {
+
+            var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
+
+            if (nota != null)
+            {
+
+                nota.IsArchived = false;
+
+                _notesContext.SaveChanges();
+            }
+            else
+            {
+
+                throw new Exception("La nota no existe para el usuario especificado.");
+            }
+        }
+        else
+        {
+
+            throw new Exception("El usuario no existe.");
+        }
+    }
+
+    public void RestoreNote(int userId, int noteId)
+    {
+
+        var usuario = _notesContext.Users.Find(userId);
+
+        if (usuario != null)
+        {
+
+            var nota = usuario.Notes.FirstOrDefault(n => n.NoteId == noteId);
+
+            if (nota != null)
+            {
+
+                nota.IsActive = true;
+
+
+                _notesContext.SaveChanges();
+            }
+            else
+            {
+
+                throw new Exception("La nota no existe para el usuario especificado.");
+            }
+        }
+        else
+        {
+
+            throw new Exception("El usuario no existe.");
+        }
+
+
+
+    }
+}
 }
